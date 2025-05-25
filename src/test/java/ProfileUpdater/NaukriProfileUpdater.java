4620cbac6dea52b08584b34fadd0bc4faa095ab9
Rajkumar Dalwai <dalwairajkumar22@gmail.com>
2025-05-26 00:10:33 +0530

    Refactor NaukriProfileUpdater as a TestNG test in src/test/java

--- src/test/java/ProfileUpdater/NaukriProfileUpdater.java
+++ src/test/java/ProfileUpdater/NaukriProfileUpdater.java
@@ -1,16 +1,26 @@
+package org.example;
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class NaukriProfileUpdater {
-    public static void main(String[] args) throws InterruptedException {
-    	
-        WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Add implicit wait
-        
+    }
+
+    @Test
+    public void testUpdateProfileHeadline() throws InterruptedException {
         driver.get("https://www.naukri.com/nlogin/login");
 
         driver.findElement(By.id("usernameField")).sendKeys("dalwairajkumar22@gmail.com");
@@ -26,11 +36,16 @@ public class NaukriProfileUpdater {
         // Clear and update headline
         By headlineBox = By.cssSelector("#resumeHeadlineTxt");
         driver.findElement(headlineBox).clear();
-        driver.findElement(headlineBox).sendKeys("Senior QA Engineer | Manual & Automation Testing: Selenium, Cypress, Appium, Rest Assured | TestNG, BDD, Cucumber | API, DB, Performance Testing | JIRA, Postman, JMeter, GitHub | CI/CD: Jenkins | SQL");
+        driver.findElement(headlineBox).sendKeys("Senior QA Engineer | Manual & Automation Testing: Selenium, Cypress, Appium, Rest Assured | TestNG, BDD, Cucumber | API, DB, Performance Testing | JIRA, Postman, JMeter, GitHub | CI/CD: Jenkins | SQL at 12.10 am");
 
         // Click Save
         driver.findElement(By.cssSelector("div[class='action s12'] button[type='submit']")).click();
+    }
 
-        driver.quit(); // Enable if you want to close the browser after update
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
